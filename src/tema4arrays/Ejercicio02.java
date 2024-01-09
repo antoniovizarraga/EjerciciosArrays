52e5d3ca13680b9cab9715f75c9bd35531017b3c
antoniovizarraga <145675344+antoniovizarraga@users.noreply.github.com>
2024-01-09 14:32:40 +0100

    Ejercicio 2 creado

---
+++ src/tema4arrays/Ejercicio02.java
@@ -0,0 +1,18 @@
+package tema4arrays;
+import java.util.Scanner;
+
+public class Ejercicio02 {
+
+	/* Vamos a crear una clase que solicite al usuario 5 valores, y los
+	 * almacene en una tabla (Array). Hay que usar un único bucle en el
+	 * que solicitamos los 5 valores y luego usamos dicho bucle para
+	 * almacenarlos en la tabla. Y en otro bucle distinto, mostrar
+	 * en pantalla los mismos números que se han introducido por
+	 * orden (foreach). */
+	public static void main(String[] args) {
+
+		
+
+	}
+
+}
