1eb84e59065efc50eec6c7ffb1ac2f12cd40cd2d
ByToxX <tonyviza@gmail.com>
2024-01-09 19:07:39 +0100

    Ejercicio 2 terminado

--- src/tema4arrays/Ejercicio02.java
+++ src/tema4arrays/Ejercicio02.java
@@ -10,9 +10,34 @@ public class Ejercicio02 {
 	 * en pantalla los mismos números que se han introducido por
 	 * orden (foreach). */
 	public static void main(String[] args) {
-
+		// Creamos la variable para que el usuario escriba el valor.
+		int userValue = 0;
 		
-
+		// Creamos el Array dónde estarán los valores.
+		int[] tabla = new int[5];
+		
+		// Creamos el Scanner.
+		Scanner sc = new Scanner(System.in);
+		
+		// Creamos el for que en el mismo bucle, pedirá los valores y los meterá en el Array.
+		/* Lo hacemos así para luego poder meter el valor en cada posición de la tabla. */
+		for(int i = 0; i < tabla.length; i++) {
+			System.out.println("Intrdozca un valor: ");
+			userValue = sc.nextInt();
+			
+			tabla[i] = userValue;
+		}
+		
+		System.out.println("Los números que se introdujeron por orden son: ");
+		
+		// Y luego en otro bucle distinto (Foreach), imprimimos los valores por orden.
+		/* Se hace así para cumplir con el ejercicio. */
+		for(int i:tabla) {
+			System.out.println(i);
+		}
+		
+		// Cerramos el Scanner.
+		sc.close();
 	}
 
 }
