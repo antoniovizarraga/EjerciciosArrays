34e9ce9eb55893a721a971c2f608b2d956e5e3bb
antoniovizarraga <145675344+antoniovizarraga@users.noreply.github.com>
2024-01-10 10:23:29 +0100

    Ejercicio 7 terminado

---
+++ src/tema4arrays/Ejercicio07.java
@@ -0,0 +1,50 @@
+package tema4arrays;
+
+import java.util.Scanner;
+
+public class Ejercicio07 {
+
+	public static void main(String[] args) {
+		// Creamos un array de Strings para almacenar los meses del año
+		String[] calendario = { "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto",
+		"Septiembre", "Octubre", "Noviembre", "Diciembre" };
+		
+		// Creamos un array de enteros para almacenar las temperaturas de cada mes
+		int[] tabla = new int[12];
+
+		// Variable para almacenar el valor introducido por el usuario
+		String userValue = "";
+
+		// Creamos un objeto Scanner para leer la entrada del usuario
+		Scanner sc = new Scanner(System.in);
+
+		// Recorremos el array de meses
+		for (int i = 0; i < calendario.length; i++) {
+
+			// Pedimos al usuario que introduzca la temperatura de cada mes
+			System.out.println(
+			"¿Qué temperatura hizo en " + calendario[i] + "? " + "(" + i + "/" + calendario.length + ")");
+			userValue = sc.nextLine();
+			// Convertimos la entrada del usuario a un entero y lo almacenamos en el array de temperaturas
+			tabla[i] = Integer.parseInt(userValue);
+		}
+		
+		System.out.println();
+		
+		// Recorremos el array de temperaturas
+		for(int i = 0; i < tabla.length; i++) {
+			// Imprimimos el nombre del mes y un separador
+			System.out.print(calendario[i] + " | ");
+			// Por cada grado de temperatura, imprimimos un asterisco
+			for(int j = 0; j < tabla[i]; j++) {
+				System.out.print("*");
+			}
+			// Pasamos a la siguiente línea
+			System.out.println();
+		}
+		
+		// Cerramos el Scanner
+		sc.close();
+	}
+
+}
